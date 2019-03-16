7d8195810d4493a7994711e02f130494b8e301d5
imyb <imyb929@gmail.com>
2019-03-16 15:58:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HTML Starter
+
+HTML Starter
---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+;(function($, window) {
+
+    'use strict';
+
+    var UI = window.UI || {};
+
+    window.UI = UI;
+
+    $(document).ready(function(){
+
+    });
+
+})(window.jQuery, window);
